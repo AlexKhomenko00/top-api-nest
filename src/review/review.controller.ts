305f65318d3b25c538f83b308d7df831e095b182
AlexSasvsha00 <githubobuczeniewaswer>
2022-04-12 19:55:26 +0200

    Create and implement create review-model  dto

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewModel } from './review.model';
 
 @Controller('review')
 export class ReviewController {
   @Post('create')
-  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+  async create(@Body() dto: CreateReviewDto) {}
 
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId') productId: string) {}
