8d6801b8f63adfa13be9c32bf58827483d55b6b9
AlexSasvsha00 <githubobuczeniewaswer>
2022-04-20 17:51:09 +0200

    Init auth service and implemented register route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,33 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ALREADY_REGISTERED_ERROR } from './auth.constants';
+import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
-  @Post('register')
-  async register(@Body() dto: AuthDto) {}
+  constructor(private readonly authService: AuthService) {}
 
+  @UsePipes(new ValidationPipe())
+  @Post('register')
+  async register(@Body() dto: AuthDto) {
+    const existingUser = await this.authService.findUser(dto.login);
+
+    if (existingUser) {
+      throw new BadRequestException(ALREADY_REGISTERED_ERROR);
+    }
+
+    return this.authService.createUser(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login')
   async login(@Body() dto: AuthDto) {}
