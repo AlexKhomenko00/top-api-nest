162239263f04db02ea3269375a052ce03e107860
AlexSasvsha00 <githubobuczeniewaswer>
2022-04-27 08:45:12 +0200

    Creted custom decorator for user email

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -12,6 +12,7 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
@@ -28,10 +29,15 @@ export class ReviewController {
 
   @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {
+  async getByProduct(
+    @Param('productId') productId: string,
+    @UserEmail() email: string,
+  ) {
+    console.log('email', email);
     return this.reviewService.findByProductId(productId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
     const deleteDoc = await this.reviewService.delete(id);
