3c8df326a9fa5601ee1b57d451b0c66fdc791418
AlexSasvsha00 <githubobuczeniewaswer>
2022-04-29 09:42:24 +0200

    Create aggregation for product findWithReviews

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from 'nestjs-typegoose';
+import { ReviewModel } from 'src/review/review.model';
+import { FindProductDto } from './dto/find-product.dto';
 import { CreateProductDto } from './dto/product.dto';
 import { ProductModel } from './product.model';
 
@@ -25,5 +27,45 @@ export class ProductService {
 
   async updateById(id: string, dto: CreateProductDto) {
     return this.productModel.findByIdAndUpdate(id, dto, { new: true }).exec();
+  }
+
+  async findWithReviews(dto: FindProductDto) {
+    return this.productModel
+      .aggregate([
+        {
+          $match: {
+            categories: dto.category,
+          },
+        },
+        {
+          $sort: {
+            _id: 1,
+          },
+        },
+        {
+          $limit: dto.limit,
+        },
+        {
+          $lookup: {
+            from: 'Review',
+            localField: '_id',
+            foreignField: 'productId',
+            as: 'reviews',
+          },
+        },
+        {
+          $addFields: {
+            reviewCount: { $size: '$reviews' },
+            reviewAvg: { $avg: '$reviews.rating' },
+          },
+        },
+      ])
+      .exec() as Promise<
+      (ProductModel & {
+        reviews: ReviewModel[];
+        reviewCount: number;
+        reviewAvg: number;
+      })[]
+    >;
   }
 }
