963f47427ad7ca5690f6625ea7b7645a4d5ee017
AlexSasvsha00 <githubobuczeniewaswer>
2022-05-17 20:00:26 +0200

    Created file module

---
+++ src/files/files.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { format } from 'date-fns';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
+import { FileElementResponse } from './dto/file-element.response.dto';
+
+@Injectable()
+export class FilesService {
+  async saveFiles(
+    files: Express.Multer.File[],
+  ): Promise<FileElementResponse[]> {
+    const dateFolder = format(new Date(), 'yyyy-MM-dd');
+    const uploadFolder = `${path}/uploads/${dateFolder}`;
+    ensureDir(uploadFolder);
+
+    const res: FileElementResponse[] = [];
+    for (const file of files) {
+      const fileName = file.originalname;
+      await writeFile(`${uploadFolder}/${fileName}`, file.buffer);
+      res.push({ name: fileName, url: `${dateFolder}/${fileName}` });
+    }
+
+    return res;
+  }
+}
