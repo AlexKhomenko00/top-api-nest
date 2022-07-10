305c0709165844c5380e9eae4364158d27056587
AlexSasvsha00 <oleksandr.khomenko2000@gmail.com>
2022-07-10 14:47:52 +0200

    Add test unused import

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserModel } from '../user.model';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
